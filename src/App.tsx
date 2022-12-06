80b4323f791cec5416886e5554570c4a5ee0c11f
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-12-06 22:48:51 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {Header} from "./Header";
+import Technologies from "./Technologies";
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+
+
+export default App;
---
+++ src/Technologies.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Technologies=()=>{
+    return (
+        <div>
+            <ul>
+                <li> CSS</li>
+                <li> HTML</li>
+                <li> REACT</li>
+            </ul>
+        </div>
+    )
+}
+export default Technologies
