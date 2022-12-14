49982696c1f026410dd431a45f948b6b824204b5
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-12-14 19:07:36 +0300

    lesson 19

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,29 @@ import {Header} from "./components/Header/Header";
 import {NavBar} from "./components/NavBar/NavBar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
-
+import {BrowserRouter, Route} from "react-router-dom";
+import {Settings} from "./components/Settinds/Settings";
+import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
 
 
 function App() {
     return (
-        <div className="appWrapper">
-
-            <Header/>
-            <NavBar/>
-           {/* <Profile/>*/}
-          <div className="appWrapperContent">
-              <Dialogs/>
-          </div>
-
-        </div>
+        <BrowserRouter>
+            <div className="appWrapper">
+                <Header/>
+                <NavBar/>
+                <div className="appWrapperContent">
+                    <Route path={'/dialogs'} component={Dialogs}/>
+                    <Route path={'/profile'} component={Profile}/>
+                    <Route path={'/news'} component={News}/>
+                    <Route path={'/music'} component={Music}/>
+                    <Route path={'/setting'} component={Settings}/>
+                    {/* <Dialogs/>
+              <Profile/>*/}
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/Music/Music.tsx
@@ -0,0 +1,12 @@
+import src from "*.bmp";
+
+import React from 'react';
+
+export const Music = () => {
+    return (
+        <div>
+          Music
+        </div>
+    );
+};
+
