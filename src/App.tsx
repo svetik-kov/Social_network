fa6ecff8ae679c9dd2fa8b97dd68b5e7897158a3
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-12-15 23:06:59 +0300

    lesson 22 Route exact

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
                 <Header/>
                 <NavBar/>
                 <div className="appWrapperContent">
-                    <Route path={'/dialogs'} component={Dialogs}/>
+                    <Route  path={'/dialogs'} component={Dialogs}/>
                     <Route path={'/profile'} component={Profile}/>
                     <Route path={'/news'} component={News}/>
                     <Route path={'/music'} component={Music}/>
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,22 +1,33 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
-  return (
-      <div className={s.dialogs}>
-        <div className={s.dialogItems}>
-          <div className={s.dialog +' '+ s.active}>Svetlana</div>
-          <div className={s.dialog}>Nina</div>
-          <div className={s.dialog}>Dima</div>
-          <div className={s.dialog}>Nadya</div>
-          <div className={s.dialog}>Olya</div>
-          <div className={s.dialog}>Marina</div>
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>Svetlana</div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialog/1'}>Nina</NavLink>
+                </div>
+                <div className={s.dialog}>
+                   <NavLink to={'//dialog/2'}>Dima</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialog/3'}>Nadya</NavLink>
+                </div>
+                <div className={s.dialog}>
+                   <NavLink to={'/dialog/4'}>Olia</NavLink>
+                </div>
+                <div className={s.dialog}>
+                   <NavLink to={'/dialog/'}>Marina</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi!!!</div>
+                <div className={s.message}> How are you?</div>
+                <div className={s.message}> You are welcome!!!</div>
+            </div>
         </div>
-        <div className={s.messages}>
-          <div className={s.message}>Hi!!! </div>
-          <div className={s.message}> How are you?</div>
-          <div className={s.message}> You are welcome!!! </div>
-        </div>
-      </div>
-  )
+    )
 }
