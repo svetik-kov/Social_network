9e9b2d6a46b04909f0ff3d04ef988d058cabdb04
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-01-27 14:27:33 +0300

    lesson 41. reducer ( switch ...case)

--- src/redux/DialogsReducer.tsx
+++ src/redux/DialogsReducer.tsx
@@ -6,15 +6,18 @@ import {ActionTypes, PostsType, StateType} from "./state";
     const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY'
     const SEND_MESSAGE = 'SEND-MESSAGE'
 
-    if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-        state.dialogsPage.newMessageBody = action.body
-
-    } else if (action.type === SEND_MESSAGE) {
-        let body = state.dialogsPage.newMessageBody
-        state.dialogsPage.newMessageBody = ''
-       state.dialogsPage.messages.push({id: 4, message: body})
-    }
-    return state
+     switch (action.type){
+         case "UPDATE-NEW-MESSAGE-BODY":
+             state.dialogsPage.newMessageBody = action.body
+             return state
+         case "SEND-MESSAGE":
+             let body = state.dialogsPage.newMessageBody
+             state.dialogsPage.newMessageBody = ''
+             state.dialogsPage.messages.push({id: 4, message: body})
+             return state
+         default:
+             return state
+     }
 };
 
 export default DialogsReducer
--- src/redux/ProfileReducer.tsx
+++ src/redux/ProfileReducer.tsx
@@ -6,20 +6,22 @@ import {ActionTypes, PostsType, StateType} from "./state";
  const ProfileReducer = (state:StateType,action:ActionTypes) => {
     const ADD_POST = 'ADD-POST'
     const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
-    if (action.type === ADD_POST) {
-        let newPost: PostsType = {
-            id: 5,
-            message: state.profilePage.newPostText,
-            likeCounter: 0
-        }
-        state.profilePage.posts.push(newPost)
-       state.profilePage.newPostText = ''
+     switch (action.type){
+         case "ADD-POST":
+             let newPost: PostsType = {
+                 id: 5,
+                 message: state.profilePage.newPostText,
+                 likeCounter: 0
+             }
+             state.profilePage.posts.push(newPost)
+             state.profilePage.newPostText = ''
+             return state
+         case "UPDATE-NEW-POST-TEXT":
+             state.profilePage.newPostText = action.newText
+             return state
+         default: return state
+     }
 
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-        state.profilePage.newPostText = action.newText
-
-    }
-return state
 };
 
 export default ProfileReducer
